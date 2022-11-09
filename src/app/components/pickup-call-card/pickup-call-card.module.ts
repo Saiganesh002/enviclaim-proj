74e31632a48248d158003c2967d0bf0d3f8c6f76
Saiganesh Jangala <saiganeshjangala01@gmail.com>
2022-11-09 13:18:56 +0530

    updated validations and ngrx for login page

--- src/app/components/pickup-call-card/pickup-call-card.module.ts
+++ src/app/components/pickup-call-card/pickup-call-card.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PickupCallCardComponent } from 'src/app/components/pickup-call-card/pickup-call-card.component';
+import { PickupCallCardComponent } from './pickup-call-card.component';
 
 @NgModule({
   imports: [CommonModule],
